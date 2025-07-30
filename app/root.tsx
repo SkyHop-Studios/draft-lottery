6acc5a3c31576f79906a90b41977ab3a3b03cedc
Matthew Hibbert <matthew.hibbert21@gmail.com>
2025-07-30 15:18:04 +0200

    styles

--- app/root.tsx
+++ app/root.tsx
@@ -3,12 +3,14 @@ import {Outlet} from "react-router";
 import "/app/app.css"
 
 export default function Root() {
-  return <div>
+  return <html>
     <head>
       <link rel="stylesheet" href="https://draft-lottery-five.vercel.app/assets/root-CkRgct3r.css"/>
     </head>
-    <div className="min-h-screen bg-black text-white font-apotek">
-      <Outlet/>
-    </div>
-  </div>
+    <body>
+      <div className="min-h-screen bg-black text-white font-apotek">
+        <Outlet/>
+      </div>
+    </body>
+  </html>
 }
