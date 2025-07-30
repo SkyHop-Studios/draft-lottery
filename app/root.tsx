e65e78e0f30b65a1dfc3f5fcbb39c4919f598e8d
Matthew Hibbert <matthew.hibbert21@gmail.com>
2025-07-30 15:19:13 +0200

    fix

--- app/root.tsx
+++ app/root.tsx
@@ -1,11 +1,12 @@
 import React from "react";
-import {Outlet} from "react-router";
+import {Links, Meta, Outlet} from "react-router";
 import "/app/app.css"
 
 export default function Root() {
   return <html>
     <head>
-      <link rel="stylesheet" href="https://draft-lottery-five.vercel.app/assets/root-CkRgct3r.css"/>
+      <Links />
+      <Meta />
     </head>
     <body>
       <div className="min-h-screen bg-black text-white font-apotek">
